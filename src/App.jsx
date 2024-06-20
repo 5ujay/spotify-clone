07fabed76506d1132228dff33b5e5fbf2e4e3783
5ujay <sujaypagam777@gmail.com>
2024-06-20 13:48:18 +0530

    update player.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Player from "./components/Player";
 import Sidebar from "./components/Sidebar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <div className="h-[90%] flex">
         <Sidebar />
       </div>
+      <Player />
     </div>
   );
 }
