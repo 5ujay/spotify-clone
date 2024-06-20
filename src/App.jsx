c8bc746c0e9b72f551b2f5b49982ad3227ce35bd
5ujay <sujaypagam777@gmail.com>
2024-06-20 12:50:28 +0530

    Sidebar UI update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <div className="h-screen bg-black">
-      <div className="h-[90%] flex"></div>
+      <div className="h-[90%] flex">
+        <Sidebar />
+      </div>
     </div>
   );
 }
